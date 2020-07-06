eb6bd0e40b606e94629369dd4b6b19f1f913d11b
Crispin Corpuz <ccorpuz@Crispins-MacBook-Pro.local>
2020-07-06 11:34:09 -0500

    Safari functionality done

--- RockPaperScissors/Controller/ViewController.swift
+++ RockPaperScissors/Controller/ViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SFSafariViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onRulesButtonPressed(_ sender: Any) {
+        let safariViewController = SFSafariViewController(url: URL(string: "https://en.wikipedia.org/wiki/Rock_paper_scissors#:~:text=A%20player%20who%20decides%20to,%22scissors%20cuts%20paper%22).")!)
+        safariViewController.delegate = self as SFSafariViewControllerDelegate
+        present(safariViewController, animated: true, completion: nil)
+    }
+    
+    
 }
 
