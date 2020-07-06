7d14e9c1f223d9178c2fd82253970a3ab8a98f89
Crispin Corpuz <ccorpuz@Crispins-MacBook-Pro.local>
2020-07-06 10:45:53 -0500

    First Commit

---
+++ RockPaperScissors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by Crispin Corpuz on 7/6/20.
+//  Copyright © 2020 Crispin Corpuz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
