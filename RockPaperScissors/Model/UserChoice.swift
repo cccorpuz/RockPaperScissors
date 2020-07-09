8250456f9cd6172ac8c206753b57ef608c762d57
Crispin Corpuz <ccorpuz@Crispins-MacBook-Pro.local>
2020-07-08 21:41:27 -0500

    Use this to control UIImages, similar to Planets class

---
+++ RockPaperScissors/Model/UserChoice.swift
@@ -0,0 +1,14 @@
+//
+//  UserChoice.swift
+//  RockPaperScissors
+//
+//  Created by Crispin Corpuz on 7/8/20.
+//  Copyright © 2020 Crispin Corpuz. All rights reserved.
+//
+
+import Foundation
+
+class UserChoice
+{
+    
+}
