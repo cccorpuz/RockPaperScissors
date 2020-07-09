ed265a04f5c8effadd20d0b95008a00447591ecd
Claire <clschweike@gmail.com>
2020-07-08 22:45:44 -0500

    Add double-tap gesture recognizers for image views

--- RockPaperScissors/Controller/ViewController.swift
+++ RockPaperScissors/Controller/ViewController.swift
@@ -10,10 +10,47 @@ import UIKit
 import SafariServices
 
 class ViewController: UIViewController, SFSafariViewControllerDelegate {
+    
+    @IBOutlet weak var rockImageView: UIImageView!
+    @IBOutlet weak var scissorsImageView: UIImageView!
+    @IBOutlet weak var paperImageView: UIImageView!
+    
+    var selectionString = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let rockGestureRecog = UIGestureRecognizer(target: self, action: #selector(tappedRock))
+        rockImageView.addGestureRecognizer(rockGestureRecog)
+        rockImageView.isUserInteractionEnabled = true
+        
+        let paperGestureRecog = UIGestureRecognizer(target: self, action: #selector(tappedPaper))
+        paperImageView.addGestureRecognizer(paperGestureRecog)
+        paperImageView.isUserInteractionEnabled = true
+        
+        let scissorsGestureRecog = UIGestureRecognizer(target: self, action: #selector(tappedScissors))
+        scissorsImageView.addGestureRecognizer(scissorsGestureRecog)
+        scissorsImageView.isUserInteractionEnabled = true
+    }
+    
+    @IBAction func tappedRock(_ sender: UIGestureRecognizer) {
+        selectionString = "rock"
+        print(selectionString)
+        
+    }
+    
+    @IBAction func tappedPaper(_ sender: UIGestureRecognizer) {
+        selectionString = "paper"
+        print(selectionString)
+    }
+    
+    @IBAction func tappedScissors(_ sender: UIGestureRecognizer) {
+        selectionString = "scissors"
+        print(selectionString)
     }
 
     @IBAction func onRulesButtonPressed(_ sender: Any) {
